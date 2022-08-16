190d5651e5961cd5297a27a1339818bef28def21
РњРёMikhail <medved-v-maline@yandex.ru>
2022-08-16 23:03:59 +0300

    S.Holmes visit card

---
+++ step3/SHolmesVisitCard.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+  setbuf(stdout, NULL);
+  printf("S.Holmes:\n");
+  printf("%igrad %i'%i.%i\" N \n",51, 31, 25, 48);
+  printf("%i grad  %i'%i.%i\" W \n",0, 9, 29, 93);
+  return 0;
+}
