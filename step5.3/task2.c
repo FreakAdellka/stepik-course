8f65570000ff269462a6e13f1bf375b531282c00
РњРёMikhail <medved-v-maline@yandex.ru>
2022-08-31 21:03:01 +0300

    finished step5.2 and started step5.3

---
+++ step5.3/task2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#include <time.h>
+
+ 
+int main(void) {
+	setbuf(stdout,NULL);
+	
+	int f, d;
+	scanf("%i %i", &f, &d);
+	int a = x / 3;
+	int b = pow(2, a);
+	
+	printf("%i", b);
+
+	return 0;
+}
