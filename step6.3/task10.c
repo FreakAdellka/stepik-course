d109c1385ada2ebe750f85d12574626447a82c54
РњРёMikhail <medved-v-maline@yandex.ru>
2022-09-28 19:49:31 +0300

    step6.3 done

--- step6.3/task10.c
+++ step6.3/task10.c
@@ -13,8 +13,32 @@ int main(void) {
 	
 	int a, b, c, o1, o2, o3;
 	scanf("%i %i %i", &a, &b, &c);
-	if((a<)&&())
+	if((a<=b)&&(a<=c)){
+		o1 = a;
+	}
+	if((b<=a)&&(b<=c)){
+		o1 = b;
+	}
+	if((c<=a)&&(c<=b)){
+		o1 = c;
+	}
+	if((a>=b)&&(a>=c)){
+		o3 = a;
+	}
+	if((b>=a)&&(b>=c)){
+		o3 = b;
+	}
+	if((c>=a)&&(c>=b)){
+		o3 = c;
+	}
+	if((o1 == a)&&(o3 == b)) o2 = c;
+	if((o1 == a)&&(o3 == c)) o2 = b;
+	if((o1 == b)&&(o3 == a)) o2 = c;
+	if((o1 == b)&&(o3 == c)) o2 = a;
+	if((o1 == c)&&(o3 == b)) o2 = a;
+	if((o1 == c)&&(o3 == a)) o2 = b;
 	
+	printf("%i %i %i", o1, o2, o3);
 	
 	
 	
